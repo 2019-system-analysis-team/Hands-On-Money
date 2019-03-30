e0d3ec124475feb4ac38c676a854d4b04ee9fb0f
popiko <3512348238@qq.com>
2019-03-30 13:19:23 +0800

    add avatar

--- bend/hand_on_money_demo/run.py
+++
@@ -1,8 +0,0 @@
-from moneyapp import app
-
-if __name__ == '__main__':
-<<<<<<< HEAD
-	app.run(debug=True)
-=======
-	app.run(debug=True, host='0.0.0.0')
->>>>>>> a4d987957e36ec7e3ab39334ce4193bff60c8ff5
