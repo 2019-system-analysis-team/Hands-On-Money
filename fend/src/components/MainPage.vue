531b503479f7de8ed6516fd27d9a584df7f1e243
cyulei <1678240316@qq.com>
2019-04-25 15:20:28 +0800

    主页面更新

--- fend/src/components/MainPage.vue
+++ fend/src/components/MainPage.vue
@@ -4,12 +4,12 @@
             <Header>
                 <Menu mode="horizontal" theme="dark">
                     <div class="layout-logo"></div>
-                    <div class="layout-nav">
+                    <div class="layout-nav" v-if="!isLogin">
                         <MenuItem name="1">
                             <Icon type="ios-navigate"></Icon>
                             项目 1
                         </MenuItem>
-                        <MenuItem name="2">
+                        <MenuItem name="2" to="/userinfomodify">
                             <Icon type="ios-keypad"></Icon>
                             项目 2
                         </MenuItem>
@@ -67,14 +67,19 @@
 			document.getElementById('bgColorDiv').style.display='block'">登录</Button>
 						<Button type="primary" ghost v-on:click="register">注册</Button>
 					</div>
-					<div>
-						<Submenu name="4" v-if="isLogin">
+					<div class="layout-button" v-if="isLogin">
+						<Submenu name="4">
 							<template slot="title">
-								<Avatar src="https://i.loli.net/2017/08/21/599a521472424.jpg" />
+								<Avatar :src="profilePhotoPath" style="background-color: #87d068">{{shownickname}}</Avatar>
 							</template>
 							<MenuItem name="4-1" to="/userinfomodify">个人信息</MenuItem>
 							<MenuItem name="4-2" @click.native="logout()">退出</MenuItem>
 						</Submenu>
+						<Badge :count="messagesNumber" overflow-count="99">
+							<a class="demo-badge" v-on:click="showMessage"> 
+								<Icon type="md-notifications-outline" size="26" color="#eee"></Icon>
+							</a>
+						</Badge>
 					</div>
                 </Menu>
             </Header>
@@ -84,44 +89,90 @@
                         Content
                     </div>
                 </Card>
+			</Content>			
+            <Content :style="{padding: '50px 50px'}" v-if="isLogin">
+				<Row v-if="haveTask">
+					<Col span="11" style="margin-left: 30px;">
+						<Card>
+							<p slot="title">已发布的任务</p>
+							<p>							
+								<div style="min-height: 300px;">
+									<p>Content of card</p>
+									<p>Content of card</p>
+									<p>Content of card</p>
+								</div>
+							</p>
+						</Card>
+					</Col>
+					<Col span="11" style="margin-left: 30px;">
+						<Card>
+							<p slot="title">已接取的任务</p>
+							<p>							
+								<div style="min-height: 300px;">
+									<p>Content of card</p>
+									<p>Content of card</p>
+									<p>Content of card</p>
+								</div>
+							</p>
+						</Card>
+					</Col>
+				</Row>
+				<Row v-if="!haveTask" style="margin-left: 30px;">
+					<Col span="23">
+						<Card>
+							<p slot="title">开始创建一个新任务吧</p>
+							<p>							
+								<div style="min-height: 300px;text-align:center;">
+									<Button type="primary" ghost shape="circle" icon="md-add" style="width: 30%;height: 63px;margin-top: 8%;">新建任务</Button>
+								</div>
+							</p>
+						</Card>
+					</Col>
+				</Row>
+				<Row style="margin-top: 30px; margin-left: 30px;">
+					<Col span="23">
+						<Card>
+							<p slot="title">任务推荐</p>
+							<p>							
+								<div style="min-height: 300px;">
+									<p>Content of card</p>
+									<p>Content of card</p>
+									<p>Content of card</p>
+								</div>
+							</p>
+						</Card>
+					</Col>
+				</Row>
             </Content>
             <Footer class="layout-footer-center">2019-2019 &copy; SYSU</Footer>
         </Layout>
-
-
-
-
-
-
-
-
-
-
-        <div id="bgColorDiv" class="black_overlay">
-			<div id="loginFrame" class="white_content">
-				<form class="form-horizontal">
-					<span class="heading">用户登录</span>
-					<div class="form-group">
-						<input type="email" class="form-control" id="inputEmail3" placeholder="username">
-						<i class="fa fa-user"></i>
-					</div>
-					<div class="form-group help">
-						<input type="password" class="form-control" id="inputPassword3" placeholder="password">
-						<i class="fa fa-lock"></i>
-						<a href="#" class="fa fa-question-circle"></a>
-					</div>
-					<div class="form-group">
-						<div class="main-checkbox">
-							<input type="checkbox" value="None" id="checkbox1" name="check">
-							<label for="checkbox1"></label>
-						</div>
-						<span class="text">记住密码</span>
-						<button type="submit" class="btn btn-default" onclick="
-				document.getElementById('bgColorDiv').style.display='none'">取消</button>
-						<button type="submit" class="btn btn-default" onclick="
-				document.getElementById('bgColorDiv').style.display='none'">登录</button>
-					</div>
-				</form>
+		<Drawer
+			title="充值"
+			v-model="topup"
+			width="400"
+			:mask-closable="true"
+			:styles="styles"
+		>
+			<Form :model="topupData">
+					<FormItem label="充值金额 : " label-position="top">
+					<InputNumber
+								:max="10000"
+								:min="1"
+								 v-model="topupData.value"
+								></InputNumber>
+					</FormItem>
+					<FormItem label="支付方式" label-position="top">
+						<Select v-model="topupData.mode">
+							<Option value="支付宝">支付宝</Option>
+							<Option value="微信支付">微信支付</Option>
+							<Option value="信用卡">信用卡</Option>
+						</Select>
+					</FormItem>
+				</Row>
+			</Form>
+			<div class="demo-drawer-footer">
+				<Button style="margin-right: 8px" @click="topup = false">取消</Button>
+				<Button type="primary" v-on:click="recharge">充值</Button>
 			</div>
 		</Drawer>
 			
@@ -155,18 +206,11 @@
 
     </div>
 </template>
-
-
-
-
-
-
-
 <script>
     export default {
 		data() {
 			return { 
-				studentID: '',
+				userID: '',
 				isLogin: false,
 				profilePhotoPath: '',
 				money: 0,
@@ -258,21 +302,12 @@
 		}
     }
 </script>
-
-
-
-
-
-
-
 <style scoped>
 .layout{
     border: 1px solid #d7dde4;
-    width: 100%;
-    height: 100%;
     background: #f5f7f9;
     position: relative;
-    border-radius: 10px;
+    border-radius: 4px;
     overflow: hidden;
 }
 .layout-logo{
@@ -286,9 +321,9 @@
     left: 20px;
 }
 .layout-nav{
-    width: 420px;
+    width: 600px;
     margin: 0 auto;
-    margin-right: 120px;
+    margin-right: 0px;
 }
 .layout-button{
     width: 200px;
@@ -298,176 +333,31 @@
 .layout-footer-center{
     text-align: center;
 }
-.black_overlay {
-	display: none;
-	position: absolute;
-	top: 0%;
-	left: 0%;
+.ivu-layout-header{
+    padding-right: 25px;
+}
+.demo-drawer-footer{
 	width: 100%;
-	height: 100%;
-	/*background-color: black;*/
-	/*-moz-opacity: 0.8;*/
-	background: rgba(0,0,0,0.8);
-	z-index: 1001;
-	opacity: .80;
-	filter: alpha(opacity=80);
-}
-.white_content {
-	border-radius: 20px;
-	display: none;
 	position: absolute;
-	top: 20%;
-	left: 30%;
-	width: 40%;
-	height: 370px;
-	padding: 16px;
-	//border: 3px solid orange;
-	background-color: white;
-	z-index: 1002;
-	overflow: auto;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-.form-bg{
-	padding: 2em 0;
-}
-.form-horizontal{
-	background: #fff;
-	padding-bottom: 40px;
-	border-radius: 15px;
-	text-align: center;
-}
-.form-horizontal .heading{
-	display: block;
-	font-size: 35px;
-	font-weight: 700;
-	padding: 20px 0;
-	border-bottom: 1px solid #f0f0f0;
-	margin-bottom: 40px;
-}
-.form-horizontal .form-group{
-	padding: 0 40px;
-	margin: 0 0 25px 0;
-	position: relative;
-}
-.form-horizontal .form-control{
-	font-size: 15px;
-	background: #f0f0f0;
-	border: none;
-	border-radius: 20px;
-	box-shadow: none;
-	padding: 0 40px 0 15px;
-	height: 40px;
-	transition: all 0.3s ease 0s;
-}
-.form-horizontal .form-control:focus{
-	background: #e0e0e0;
-	box-shadow: none;
-	outline: 0 none;
-}
-.form-horizontal .form-group i{
-	position: absolute;
-	top: 12px;
-	left: 60px;
-	font-size: 17px;
-	color: #c8c8c8;
-	transition : all 0.5s ease 0s;
-}
-.form-horizontal .form-control:focus + i{
-	color: #00b4ef;
-}
-.form-horizontal .fa-question-circle{
-	display: inline-block;
-	position: absolute;
-	top: 12px;
-	right: 60px;
-	font-size: 20px;
-	color: #808080;
-	transition: all 0.5s ease 0s;
-}
-.form-horizontal .fa-question-circle:hover{
-	color: #000;
-}
-.form-horizontal .main-checkbox{
-	float: left;
-	left: 20px;
-	width: 20px;
-	height: 20px;
-	background: #11a3fc;
-	border-radius: 50%;
-	position: relative;
-	margin: 12px 0 0 5px;
-	border: 1px solid #11a3fc;
-}
-.form-horizontal .main-checkbox label{
-	width: 20px;
-	height: 20px;
-	position: absolute;
-	top: 0;
+	bottom: 50%;
 	left: 0;
-	cursor: pointer;
+	border-top: 1px solid #e8e8e8;
+	padding: 10px 16px;
+	text-align: right;
+	background: #fff;
 }
-.form-horizontal .main-checkbox label:after{
-	content: "";
-	width: 10px;
-	height: 5px;
-	position: absolute;
-	top: 5px;
-	left: 4px;
-	border: 3px solid #fff;
-	border-top: none;
-	border-right: none;
-	background: transparent;
-	opacity: 0;
-	-webkit-transform: rotate(-45deg);
-	transform: rotate(-45deg);
+.demo-badge{
+	width: 30px;
+	height: 30px;
+	background: #515a6e;
+	border-radius: 6px;
+	display: inline-block;
 }
-.form-horizontal .main-checkbox input[type=checkbox]{
-	visibility: hidden;
+.ivu-badge{
+	margin-top: 15px;
 }
-.form-horizontal .main-checkbox input[type=checkbox]:checked + label:after{
-	opacity: 1;
-}
-.form-horizontal .text{
-	float: left;
-	margin-top: 7px;
-	margin-left: 25px;
-	line-height: 20px;
-	padding-top: 5px;
-	text-transform: capitalize;
-}
-.form-horizontal .btn{
-	float: right;
-	font-size: 14px;
-	color: #fff;
-	background: #00b4ef;
-	border-radius: 30px;
-	padding: 10px 25px;
-	margin-left: 10px;
-	border: none;
-	text-transform: capitalize;
-	transition: all 0.5s ease 0s;
-}
-@media only screen and (max-width: 479px){
-	.form-horizontal .form-group{
-		padding: 0 25px;
-	}
-	.form-horizontal .form-group i{
-		left: 45px;
-	}
-	.form-horizontal .btn{
-		padding: 10px 20px;
-	}
+.ivu-icon-md-notifications-outline{
+	margin-bottom: 30px;
 }
 
 
